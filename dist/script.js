b0d76547552e35648a36cda48c1ae93a69e2f85a
dluca22 <luc.due@tim.it>
2023-03-10 10:25:05 +0100

    base html

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,1 @@
 "use strict";
-console.log("ciao");
-const body = document.querySelector("body");
-const header = document.createElement("h1");
-header.innerText = "header ciao";
-body === null || body === void 0 ? void 0 : body.append(header);
--- src/script.ts
+++
@@ -1,9 +0,0 @@
-console.log("ciao")
-
-const body = document.querySelector("body")
-
-const header: HTMLElement = document.createElement("h1")
-header.innerText = "header ciao"
-
-body?.append(header)
-
