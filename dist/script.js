9c781c5236c2a59105e0566474206357690c82ce
dluca22 <luc.due@tim.it>
2023-03-10 11:02:32 +0100

    testing formData

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,20 @@
 "use strict";
+const form = document.querySelector("#input-form");
+/*
+added this code to tsconfig to allow Object.fromEntries to iterate over DOM objects
+"lib": [
+  "es2019",
+  "dom",
+  "dom.iterable"
+],
+
+
+
+*/
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+    console.log(data.name);
+    form.reset();
+});
---
+++ src/script.ts
@@ -0,0 +1,23 @@
+
+
+const form = document.querySelector("#input-form")as HTMLFormElement;
+
+
+/*
+added this code to tsconfig to allow Object.fromEntries to iterate over DOM objects
+"lib": [
+  "es2019",
+  "dom",
+  "dom.iterable"
+],
+
+
+
+*/
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    const formData = new FormData(form)
+    const data = Object.fromEntries(formData)
+    console.log(data.name)
+    form.reset()
+})
