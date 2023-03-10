c246b9888a3bf20697eef5a03d57dd17c14d4f91
dluca22 <luc.due@tim.it>
2023-03-10 09:13:39 +0100

    initial dom manipulation setup

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log("working");
+console.log("ciao");
+const body = document.querySelector("body");
+const header = document.createElement("h1");
+header.innerText = "header ciao";
+body === null || body === void 0 ? void 0 : body.append(header);
